f977486ce8ad2a363ee0df8f59b705312f286102
conor-mcgowan <conor.mcgowan@live.com>
2021-02-16 14:35:39 -0600

    sets up routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from "react";
 import "./App.css";
+import React from "react";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  NavLink,
+} from "react-router-dom";
+import LoginPage from "./components/Login/LoginPage";
+import SearchPage from "./components/Search/SearchPage";
+import FavoritesPage from "./components/Favorites/FavoritesPage";
 
 function App() {
   return (
     <>
-      <nav></nav>
-      <main></main>
-      <footer>This is a footer</footer>
+      <Router>
+        <nav>
+          <NavLink to="/login" activeClassName="active-link">
+            Login
+          </NavLink>
+          <NavLink to="/search" activeClassName="active-link">
+            Search
+          </NavLink>
+          <NavLink to="/favorites" activeClassName="active-link">
+            Favorites
+          </NavLink>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/search" component={SearchPage} />
+            <Route exact path="/favorites" component={FavoritesPage} />
+            <Route path="*">
+              <Redirect to="/search" />
+            </Route>
+          </Switch>
+        </main>
+        <footer>This is a footer</footer>
+      </Router>
     </>
   );
 }
--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Loginage = (props) => {
+const LoginPage = (props) => {
   return <h1>LoginPage Works</h1>;
 };
 
