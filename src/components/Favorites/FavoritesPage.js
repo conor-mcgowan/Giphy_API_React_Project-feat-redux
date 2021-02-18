0b0533fda269e9c70f8c624032644aa6514e4430
conor-mcgowan <conor.mcgowan@live.com>
2021-02-18 13:39:48 -0600

    adds favoriteReducer

--- src/components/Favorites/FavoritesPage.js
+++ src/components/Favorites/FavoritesPage.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import GifDisplay from "../Gif/GifDisplay";
 
 const FavoritesPage = (props) => {
-  return <h1>FavoritesPage Works</h1>;
+  const [favorites, setFavorites] = useState([]);
+  return (
+    <>
+      <h1 className="text-center">Favorites</h1>
+      <button>Add to Favorites</button>
+      <button>Remove from Favorites</button>
+      <div>
+        {favorites.map((v) => (
+          <GifDisplay key={v.id} gif={v} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default FavoritesPage;
